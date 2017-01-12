f87294bc69299c2061edb15709bda5f038a3bcd3
premnathtg <premnath.tg@gmail.com>
2017-01-12 18:45:40 +0530

    User added first

--- push.sh
+++ push.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 if [ "$IS_PULL_REQUEST" != true ]; then
-  sudo docker push 680976004409.dkr.ecr.us-east-1.amazonaws.com/devimages:$BRANCH.$BUILD_NUMBER_$USER
+  sudo docker push 680976004409.dkr.ecr.us-east-1.amazonaws.com/devimages:_$USER.$BUILD_NUMBER
 else
   echo "skipping because it's a PR"
 fi
