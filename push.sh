7a4f918baacae409c5562cbb0a26e2ff59985376
premnathtg <premnath.tg@gmail.com>
2017-01-12 18:58:18 +0530

    modified

--- push.sh
+++ push.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 if [ "$IS_PULL_REQUEST" != true ]; then
-  sudo docker push 680976004409.dkr.ecr.us-east-1.amazonaws.com/devimages:$USER.$BUILD_NUMBER
+  sudo docker push $IMAGE_NAME:$USER.$BUILD_NUMBER
 else
   echo "skipping because it's a PR"
 fi
