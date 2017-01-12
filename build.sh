07850c8cc24b45bdb38a5f6a7f7c2d831a4f4bc6
premnathtg <premnath.tg@gmail.com>
2017-01-12 15:51:00 +0530

    New Build file added

--- build.sh
+++ build.sh
@@ -1,1 +1,6 @@
-sudo docker build -t $IMAGE_NAME:$BRANCH.$BUILD_NUMBER .
+#!/bin/bash -e
+if [ "$IS_PULL_REQUEST" != true ]; then
+  sudo docker build -t $IMAGE_NAME:$BRANCH.$BUILD_NUMBER .
+else
+  echo "skipping because it's a PREM"
+fi
