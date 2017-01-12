9449d461e31fd13e944ab5bb31628c4ceee75be5
premnathtg <premnath.tg@gmail.com>
2017-01-12 15:34:19 +0530

    removed lines

--- build.sh
+++ build.sh
@@ -1,6 +1,2 @@
 #!/bin/bash -e
-if [ "$IS_PULL_REQUEST" != true ]; then
-  sudo docker build -t $IMAGE_NAME:$BRANCH.$BUILD_NUMBER .
-else
-  echo "skipping because it's a PR"
-fi
+sudo docker build -t $IMAGE_NAME:$BRANCH.$BUILD_NUMBER .
